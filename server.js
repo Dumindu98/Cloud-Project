b07e62d6575fd33786a03993d54b67335a62afa6
Dumindu Shamika <53207557+Dumindu98@users.noreply.github.com>
2021-11-27 13:45:14 +0530

    server

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const todoRoutes = express.Router();
+const PORT = 4000;
+
+let Todo = require('./model');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://mongo:27017/test', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("data is not found");
+        else
+            todo.todo_Owner_name  = req.body.todo_Owner_name ;
+            todo.todo_date = req.body.todo_date;
+            todo.todo_Car_model = req.body.todo_Car_model;
+            todo.todo_Manufacture_year = req.body.todo_Manufacture_year;
+            todo.todo_Fuel_type = req.body.todo_Fuel_type;
+            todo.todo_Mileage = req.body.todo_Mileage;
+            todo.todo_Description = req.body.todo_Description;
+
+            todo.save().then(todo => {
+                res.json('Todo updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+
+todoRoutes.route('/add').post(function(req, res) {
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
